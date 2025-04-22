581872e06b607740e199370d24fe2d99e30d69d4
Yassel Izquierdo <sioy23@gmail.com>
2025-04-22 14:26:51 -0300

    Configuración de Nginx para yoisar.com y ajustes en rutas del backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PresentacionYassel from './App';
-import './index.css'; // Asegúrate de tener estilos globales si es necesario
 
 ReactDOM.render(<PresentacionYassel />, document.getElementById('root'));
